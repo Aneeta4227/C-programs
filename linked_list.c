d067402be2ad2337a709a70bac88bea589a63dc2
Aneeta Jose <115390098+AneetaJose@users.noreply.github.com>
2022-11-21 21:48:07 +0530

    Update linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -24,6 +24,10 @@ void insert_pos()
     int pos,i=0;
     printf("Enter the position to insert: ");
     scanf("%d",&pos);
+    if(pos==0)
+        insert_front();
+    else
+    {
     read();
     temp=head;
     while(i<pos)
@@ -34,6 +38,7 @@ void insert_pos()
     }
     prev->link=newnode;
     newnode->link=temp;
+    }
 }
 void insert_end()
 {
@@ -47,18 +52,67 @@ void insert_end()
     prev->link=newnode;
     newnode->link=0;
 }
+void delete_front()
+{
+    if(head==0)
+        printf("List Empty\n");
+    else
+    {
+        temp=head;
+        head=head->link;
+        free(temp);
+    }
+}
+void delete_pos()
+{
+    int pos,i=0;
+    printf("Enter the position to be deleted: ");
+    scanf("%d",&pos);
+    if(pos==0)
+        delete_front();
+    else
+    {
+        temp=head;
+        while(i<pos)
+        {
+            prev=temp;
+            temp=prev->link;
+            i++;
+        }
+        prev->link=temp->link;
+        free(temp);
+    }
+}
+void delete_end()
+{
+    if(head==0)
+        printf("List Empty\n");
+    else
+    {
+        temp=head;
+        while(temp->link!=0)
+        {
+            prev=temp;
+            temp=temp->link;
+        }
+        free(temp);
+        prev->link=0;
+    }
+}
 void display()
 {
+    printf("--LIST DATA--\n")
     temp=head;
     while(temp!=0)
     {
         printf("%d\t",temp->data);
         temp=temp->link;
     }
+    printf("\n");
 }
 int main()
 {
-    int n,i;
+    int n,choice,i;
     head=0;
     printf("Enter the number of nodes to be created: ");
     scanf("%d",&n);
@@ -74,8 +128,32 @@ int main()
             temp=newnode;
         }
     }
-    insert_front();
-    insert_pos();
-    insert_end();
-    display();
+    do
+    {
+        printf("Options\n1.Insertion at beginning\t2.Insertion at any position\t3.Insertion at end\n");
+        printf("4.Deletion from beginning\t5.Deletion from any position\t6.Deletion from end\n7.Display\t\t\t8.Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1: insert_front();
+                    break;
+            case 2: insert_pos();
+                    break;
+            case 3: insert_end();
+                    break;
+            case 4: delete_end();
+                    break;
+            case 5: delete_pos();
+                    break;
+            case 6: delete_end();
+                    break;
+            case 7: display();
+                    break;
+            case 8: printf("Terminated...\n");
+                    break;
+            default: printf("Invalid Choice\n");
+                    break;
+        }
+    } while (choice!=8);
 }
